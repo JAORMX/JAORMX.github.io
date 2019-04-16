35228bff524a84a427d5dad645d8b546a0edf9a8
Juan Antonio Osorio Robles <jaosorior@redhat.com>
2019-04-16 16:18:24 +0300

    Fix pep8 warnings in create_post.py script

--- create_post.py
+++ create_post.py
@@ -1,11 +1,13 @@
-import datetime
-import dateutil.tz
-import jinja2
 import os
 import sys
+import datetime
+
+import dateutil.tz
+import jinja2
 
 DEFAULT_POSTS_PATH = './_posts'
 POST_TEMPLATE = 'post.template'
+
 
 def get_title():
     try:
@@ -13,18 +15,22 @@ def get_title():
     except IndexError:
         raise RuntimeError("You need to add the title as a parameter")
 
+
 def format_title(title):
     return title.lower().replace(" ", "-")
+
 
 def get_file_path(formatted_title):
     today = str(datetime.date.today())
     return os.path.join(DEFAULT_POSTS_PATH,
                         today + '-' + formatted_title + ".markdown")
 
+
 def get_current_day():
     local_tz = dateutil.tz.tzlocal()
     current_day = datetime.datetime.now(local_tz)
     return str(current_day.date()) + current_day.strftime(" %X %z")
+
 
 def get_rendered_content(title):
     path = os.path.dirname(os.path.abspath(__file__))
@@ -39,13 +45,19 @@ def get_rendered_content(title):
     }
     return template_environment.get_template(POST_TEMPLATE).render(context)
 
+
 def write_post(content, file_path):
     with open(file_path, 'w+') as post:
         post.write(content)
 
-if __name__ == '__main__':
+
+def main():
     title = get_title()
     file_path = get_file_path(format_title(title))
     content = get_rendered_content(title)
     write_post(content, file_path)
     print(file_path)
+
+
+if __name__ == '__main__':
+    main()
